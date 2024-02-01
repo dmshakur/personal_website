c152f6fe18db2e091af8ac3dffa5ac09ef2ec658
dmshakur <dangeloshakur@gmail.com>
2024-02-01 12:07:41 -0500

    Bug where jupyter notebooks would not show up in project view fixed, however there may be an iddue with the tool itself and not being able to pull down larger notebooks."

--- app.py
+++ app.py
@@ -1,39 +1,49 @@
-from flask import Flask, render_template
+from ast import literal_eval as str_to_dict
 import yaml
+
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html', config = config)
 
-@app.route('/student_mental_health.html')
-def student_mental_health():
-    return render_template('projects/student_mental_health.html', config = config)
 
-@app.route('/spotify_data_analysis.html')
-def spotify_data_analysis():
-    return render_template('projects/spotify_data_analysis.html', config = config)
 
-@app.route('/indian_food_data_analysis.html')
-def indian_food_data_analysis():
-    return render_template('projects/indian_food_data_analysis.html', config = config)
+@app.route('/projects/<project_url>.html')
+def open_project_view(project_url):
+    project_name = request.args.get('project_name')
+    github_data = str_to_dict(request.args.get('github_data'))
+    if github_data['raw_file_display']:
+        if type(github_data['files']) is str:
+            github_data['files'] = [github_data['files']]
+    else:
+        if type(github_data['notebooks']) is str:
+            github_data['notebooks'] = [github_data['notebooks']]
+    
 
-@app.route('/ultra_marathons_data_analysis.html')
-def ultra_marathons_data_analysis():
-    return render_template('projects/ultra_marathons_data_analysis.html', config = config)
+    return render_template(
+        'project_view.html', 
+        config = config, 
+        project_name = project_name,
+        github_data = github_data
+    )
 
-@app.route('/audio_library_organizer.html')
-def audio_library_organizer():
-    return render_template('projects/audio_library_organizer.html', config = config)
+
 
 @app.after_request
 def add_header(response):
     response.headers['Cache-Control'] = 'no-store'
     return response
+
+
 
 if __name__ =='__main__':
     app.run(debug=True)
