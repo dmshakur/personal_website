3f6f6f300f8a7aad640484454538b013379a28fb
dmshakur <dangeloshakur@gmail.com>
2024-02-02 13:47:13 -0500

    Completed the code view for raw files. The buttons in project view properly work. The readme view aldo works now.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from ast import literal_eval as str_to_dict
-import yaml, json
+import json
+import yaml
 
 from flask import Flask, render_template, request
+from markdown import markdown
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+import requests
 
 
 app = Flask(__name__)
@@ -9,7 +15,6 @@ app = Flask(__name__)
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
-print('type of config: ', type(config))
 
 @app.route('/')
 def index():
@@ -40,10 +45,13 @@ def open_project_view(project_url):
 def readme():
     github_data = request.args.get('github_data')
     github_data = json.loads(github_data)
-    return render_template(
-        'readme.html',
-        github_data = github_data
-    )
+    readme_url = f'https://raw.githubusercontent.com/dmshakur/{github_data["repo_name"]}/{github_data["branch"]}/README.md'
+
+    readme = requests.get(readme_url)
+    readme_html = markdown(readme.text)
+
+    return readme_html, 200
+
 
 @app.route('/dashboard')
 def dashboard():
@@ -58,6 +66,31 @@ def dashboard():
 def code():
     github_data = request.args.get('github_data')
     github_data = json.loads(github_data)
+
+    if github_data['raw_file_display']:
+        formatter = HtmlFormatter(style = 'friendly', full = True, cssclass = 'pygments')
+
+        title = github_data['repo_name'].replace('_', ' ').capitalize()
+        h3_title = f'<h3>{title}</h3>'
+        html = [h3_title]
+
+        for i, file in enumerate(github_data['files']):
+            file_url = f'https://raw.githubusercontent.com/dmshakur/{github_data["repo_name"]}/{github_data["branch"]}/{file}'
+            python_file = requests.get(file_url)
+            highlighted_file = highlight(python_file.text, PythonLexer(), formatter)
+
+            
+            html.extend([
+                f'<h5>File {i + 1}: {file}</h5>',
+                '<hr>' + highlighted_file + '<hr>'
+            ])
+        
+        css = formatter.get_style_defs('.pygments')
+        style = f'<style>{" ".join(css)}</style>'
+        html = ' '.join(html)
+
+        return style + html, 200
+
     return render_template(
         'code.html',
         github_data = github_data
